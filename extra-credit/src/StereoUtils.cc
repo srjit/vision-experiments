41083f354e3d12fc79fbce4cb2a93c2ab6047a70
Sreejith Sreekumar <sreejith2904@gmail.com>
2018-12-11 14:34:30 -0500

    create patches

--- extra-credit/src/StereoUtils.cc
+++ extra-credit/src/StereoUtils.cc
@@ -1,11 +1,48 @@
 #include <iostream>
+#include <cmath>
 
 #include <opencv2/opencv.hpp>
 #include <opencv2/highgui.hpp>
 
 using namespace cv;
 
-void getCorrespondingPoint(int x, int y, cv::Mat fundamentalMatrix){
+
+void getMaxCorrespondenceFromLine(int rightX,
+				  int rightY,
+				  std::vector<std::vector<int>> leftPoints,
+				  cv::Mat leftImage,
+				  cv::Mat rightImage){
+
+      int x = rightX - 9 + 20;
+      int y = rightY - 9 + 20;
+
+      cv::Rect patchRegion(x, y, 21, 21);
+      cv::Mat rightPatch  = rightImage(patchRegion);
+
+      for(int pointIndex=0; pointIndex <= leftPoints.size(); pointIndex++){
+
+	int leftImgXTmp = leftPoints[pointIndex][0];
+	int leftImgYTmp = leftPoints[pointIndex][1];
+
+	int xPatchLeftBegin = leftImgXTmp - 9 + 20;
+	int yPatchLeftBegin = leftImgYTmp - 9 + 20;
+	
+
+	cv::Rect patchRegion(xPatchLeftBegin, yPatchLeftBegin, 21, 21);	
+	
+      }
+      
+      
+  
+}
+
+void getCorrespondingPoint(int x,
+			   int y,
+			   int height,
+			   int width,
+			   cv::Mat fundamentalMatrix,
+			   cv::Mat leftImage,
+			   cv::Mat rightImage){
 
   double f11 = fundamentalMatrix.at<double>(Point(0,0));
   double f12 = fundamentalMatrix.at<double>(Point(1,0));
@@ -19,6 +56,35 @@ void getCorrespondingPoint(int x, int y, cv::Mat fundamentalMatrix){
   double f32 = fundamentalMatrix.at<double>(Point(1,2));
   double f33 = fundamentalMatrix.at<double>(Point(2,2));    
 
+  /**
+   * Co-efficients of the line
+   */
+  double a = (f11 * x) + (f12 * y) + f13;
+  double b = (f21 * x) + (f22 * y) + f23;
+  double c = (f31 * x) + (f32 * y) + f33;
+
+  /**
+   * get two points on this line
+   */
+  double x_min = 0;
+
+  std::vector<std::vector<int>> points;
+
+  for(int colIndex=0; colIndex<width; colIndex++){
+
+    std::vector<int> co_ordinates;
+      
+    int rowIndex = floor((-c - (a * colIndex))/b);
+
+    co_ordinates.push_back(colIndex);
+    co_ordinates.push_back(rowIndex);
+
+    points.push_back(co_ordinates);
+    
+  }
+
+
+  getMaxCorrespondenceFromLine(x, y, points, leftImage, rightImage);
   
   
   
@@ -40,6 +106,14 @@ void getDisparityMap(cv::Mat leftImage,
 
   for(int i=0; i<height; i++){
     for(int j=0; j<width; j++){
+
+      getCorrespondingPoint(i,
+			    j,
+			    height,
+			    width,
+			    fundamentalMatrix,
+			    leftImage,
+			    rightImage);
       
       
     }
